e48b7d629afa01b0a9d13801d824b81c4d7a9eb1
HwangHyejeong <hwanghj1022@gmail.com>
2020-02-18 00:55:13 +0900

    Update LEAP_MAX_DAYS calendar

--- src/hhj/calendar/Calendar.java
+++ src/hhj/calendar/Calendar.java
@@ -1,13 +1,25 @@
 package hhj.calendar;
 
-import java.util.Scanner;
-
 public class Calendar {
-	
+	//평년
 	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	//윤년
+	private static final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	
-	public int getMaxDaysOfMonth(int month) {
-		return MAX_DAYS[month - 1];
+	public boolean isLeapYear(int year) {
+		if(year % 4 == 0 && (year % 100 != 0 || year == 400)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public int getMaxDaysOfMonth(int year, int month) {
+		if(isLeapYear(year)) {
+			return LEAP_MAX_DAYS[month - 1];
+		} else {
+			return MAX_DAYS[month - 1];
+		}
 	}
 	
 	/**
@@ -18,16 +30,31 @@ public class Calendar {
 	 * - 프롬프트를 출력한다.
 	 */
 	
-	public void printCalendar(int year, int month) {
+	public void printCalendar(int year, int month, int weekday) {
 		System.out.printf("   <<%4d년%3d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
-		System.out.println("--------------------");
+		System.out.println("---------------------");
 		
-		int maxDay = getMaxDaysOfMonth(month);
+		// print blank space
+		for (int i = 0; i < weekday; i++) {
+			System.out.print("   ");
+		}
 		
-		for(int i = 1; i <= maxDay; i++) {
+		int maxDay = getMaxDaysOfMonth(year, month);
+		int count = 7 - weekday;
+		int delim = count < 7 ? count : 0;
+		
+		// print first line
+		for (int i = 1; i <= count; i++) {
 			System.out.printf("%3d", i);
-			if( i % 7 == 0) {
+		}
+		System.out.println();
+		
+		// print from second line to last
+		count++;
+		for(int i = count; i <= maxDay; i++) {
+			System.out.printf("%3d", i);
+			if( i % 7 == delim) {
 				System.out.println();
 			}
 		}
