d015d2f0a54f3b343d34a88f3b49474699c1b899
HwangHyejeong <hwanghj1022@gmail.com>
2020-02-17 19:32:48 +0900

    Sum arrange line, Print virtual calendar shape

--- src/hhj/calendar/Sum.java
+++ src/hhj/calendar/Sum.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Sum {
 	public static void main(String[] args) {
-		int a,b;
+		int a, b;
 
 		// 입력 : 키보드로 두 수를 입력 받는다.
 		Scanner scanner = new Scanner(System.in);
@@ -14,10 +14,10 @@ public class Sum {
 		s2 = scanner.next();
 		a = Integer.parseInt(s1);
 		b = Integer.parseInt(s2);
-		
+
 		// 출력 : 화면에 두 수의 합을 출력한다.
-		//System.out.println("두 수의 합은 " + (a + b) +"입니다");
-		System.out.printf("%d와 %d의 합은 %d입니다.", a, b, a+b);
+		// System.out.println("두 수의 합은 " + (a + b) +"입니다");
+		System.out.printf("%d와 %d의 합은 %d입니다.", a, b, a + b);
 		scanner.close();
 	}
 }
