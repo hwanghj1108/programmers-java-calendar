834689466506a5e876f2e626447493c7f1417de2
HwangHyejeong <hwanghj1022@gmail.com>
2020-02-19 17:20:20 +0900

    Implement printing calendar (auto version)

--- src/hhj/calendar/Calendar.java
+++ src/hhj/calendar/Calendar.java
@@ -1,10 +1,39 @@
 package hhj.calendar;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+
 public class Calendar {
 	//평년
-	private static final int[] MAX_DAYS = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private static final int[] MAX_DAYS = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	//윤년
-	private static final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private static final int[] LEAP_MAX_DAYS = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	
+	private HashMap<Date, String> planMap;
+	
+	public Calendar() {
+		planMap = new HashMap<Date, String>();
+	}
+	
+	/**
+	 * 
+	 * @param date ex : "2017-06-20"
+	 * @param plan
+	 * @throws ParseException 
+	 */
+	public void registerPlan(String strDate, String plan) throws ParseException {
+		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
+		planMap.put(date, plan);
+	}
+	
+	public String searchPlan(String strDate) throws ParseException {
+		Date date = new SimpleDateFormat("yyyy-MM-dd").parse(strDate);
+		String plan = planMap.get(date);
+		
+		return plan;
+	}
 	
 	public boolean isLeapYear(int year) {
 		if(year % 4 == 0 && (year % 100 != 0 || year == 400)) {
@@ -16,9 +45,9 @@ public class Calendar {
 
 	public int getMaxDaysOfMonth(int year, int month) {
 		if(isLeapYear(year)) {
-			return LEAP_MAX_DAYS[month - 1];
+			return LEAP_MAX_DAYS[month];
 		} else {
-			return MAX_DAYS[month - 1];
+			return MAX_DAYS[month];
 		}
 	}
 	
@@ -30,10 +59,14 @@ public class Calendar {
 	 * - 프롬프트를 출력한다.
 	 */
 	
-	public void printCalendar(int year, int month, int weekday) {
-		System.out.printf("   <<%4d년%3d월>>\n", year, month);
+	//public void printCalendar(int year, int month, int weekday) {
+	public void printCalendar(int year, int month) {
+		System.out.printf("   <<%d년%d월>>\n", year, month);
 		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
+		
+		// get weekday automatically
+		int weekday = getWeekday(year, month, 1);
 		
 		// print blank space
 		for (int i = 0; i < weekday; i++) {
@@ -65,6 +98,43 @@ public class Calendar {
 //		System.out.println(" 8  9 10 11 12 13 14");
 //		System.out.println("15 16 17 18 19 20 21");
 //		System.out.println("22 23 24 25 26 27 28");
+	}
+
+	private int getWeekday(int year, int month, int day) {
+		int syear = 1970;
+		final int STANDARD_WEEKDAY = 4; // 1970/Jan/1st = Thursday
+		
+		// 윤년은 366, 평년은 365
+		int count = 0;
+		
+		for (int i = syear; i < year; i++) {
+			int delta = isLeapYear(i) ? 366 : 365;
+			count += delta;
+		}
+		
+		//System.out.println(count);
+		for (int i = 1; i < month; i++) {
+			int delta = getMaxDaysOfMonth(year, i);
+			count += delta;
+		}
+		
+		count += day - 1;
+		
+		int weekday = (count + STANDARD_WEEKDAY) % 7;
+		return weekday;
+	}
+	
+	// simple test code here
+	public static void main(String[] args) throws ParseException {
+		Calendar cal = new Calendar();
+		System.out.println(cal.getWeekday(1970, 1, 1) == 4);
+		System.out.println(cal.getWeekday(1971, 1, 1) == 5);
+		System.out.println(cal.getWeekday(1972, 1, 1) == 6);
+		System.out.println(cal.getWeekday(1973, 1, 1) == 1);
+		System.out.println(cal.getWeekday(1974, 1, 1) == 2);
+		
+		cal.registerPlan("2017-06-23", "Let's eat beef!");
+		System.out.println(cal.searchPlan("2017-06-23").equals("Let's eat beef!"));
 	}
 	
 }
